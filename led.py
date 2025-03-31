a92b61a73bf8c7e7366ad8382a7c552ded87ef33
NJValin <neilvalin1@gmail.com>
2025-03-31 15:22:14 -0400

    Added led.py

---
+++ led.py
@@ -0,0 +1,31 @@
+import lgpio as gpio
+import time
+
+
+class led:
+    def __init__(self, gpio_pin) -> None:
+        self.gpio_pin = gpio_pin
+        self.gpio_chip = None
+        
+        self.__gpio_init()
+
+    
+    def __gpio_init(self):
+        """
+        Initialize GPIO using `lgpio`.
+        """
+        self.gpio_chip = gpio.gpiochip_open(0)
+        gpio.gpio_claim_output(self.gpio_chip, self.gpio_pin)
+        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 0)
+
+    
+    def turn_on(self, duration=-1.0):
+        if duration == -1:
+            gpio.gpio_write(self.gpio_chip, self.gpio_pin, 1)
+        else:
+            gpio.gpio_write(self.gpio_chip, self.gpio_pin, 1)
+            time.sleep(duration)
+            self.turn_off()
+
+    def turn_off(self):
+        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 0)
--- led_test.py
+++ led_test.py
@@ -1,4 +1,4 @@
-import gpiod
+
 import time
 
 # Pin configuration
@@ -6,17 +6,5 @@ LED_PIN = 18  # Change this to the GPIO pin your LED is connected to
 CHIP_NAME = "gpiochip0"  # Default GPIO chip name
 
 # Initialize GPIO chip and line
-chip = gpiod.Chip(CHIP_NAME)
-line = chip.get_line(LED_PIN)
-line.request(consumer="led_blink", type=gpiod.LINE_REQ_DIR_OUT)
 
-try:
-    while True:
-        line.set_value(1)  # Turn LED on
-        time.sleep(1)  # Wait 1 second
-        line.set_value(0)  # Turn LED off
-        time.sleep(1)  # Wait 1 second
-except KeyboardInterrupt:
-    print("\nExiting and releasing GPIO...")
-    line.release()  # Release the GPIO line
 
