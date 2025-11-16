f25e77a575858a84b780c6621317e1b8b21a272f
Neil Valin <neilvalin1@gmail.com>
2025-11-16 14:37:28 -0500

    Added better functionality to led

--- led.py
+++ led.py
@@ -4,33 +4,39 @@ import threading
 
 
 class led:
-    def __init__(self, gpio_pin) -> None:
+    def __init__(self, gpio_pin, active_high = 1, default = 0) -> None:
         self.gpio_pin = gpio_pin
         self.gpio_chip = None
+        self.active_high = active_high
+        self.default = default
         
-        self.__gpio_init()
+        
+        self.__gpio_init(self.default)
 
     
-    def __gpio_init(self):
+    def __gpio_init(self, default):
         """
         Initialize GPIO using `lgpio`.
         """
         self.gpio_chip = gpio.gpiochip_open(0)
         gpio.gpio_claim_output(self.gpio_chip, self.gpio_pin)
-        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 1)
+        gpio.gpio_write(self.gpio_chip, self.gpio_pin, default)
 
     
     def turn_on(self, duration=-1.0):
-        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 0)
+        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 1 if self.active_high else 0)
         if duration != -1:
             threading.Thread(target=self.__turn_off_auto, args=[duration], daemon=True).start()
 
     def __turn_off_auto(self, duration):
          time.sleep(duration)
          self.turn_off()
+    
+    def read_value(self):
+        return gpio.gpio_read(self.gpio_chip, self.gpio_pin)
 
     def turn_off(self):
-        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 1)
+        gpio.gpio_write(self.gpio_chip, self.gpio_pin, 0 if self.active_high else 1)
 
     def cleanup(self):
         gpio.gpiochip_close(self.gpio_chip)
