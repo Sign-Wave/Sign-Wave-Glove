94903a7fbbcb9b11d4e1584e2e7aaf559356de77
Neil Valin <neilvalin1@gmail.com>
2025-11-22 19:46:07 -0500

    Updates

--- test2.py
+++ test2.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv("sign_language_data.csv")
 
 # Remove rows where label == "L" and flex0 > 950
-df_cleaned = df[~((df["label"] == "L") & (df["flex1"] > 850))]
+df_cleaned = df[~((df["label"] == "D") & (df["flex0"] < 730))]
 
 # Save back to the same file (overwrite)
 df_cleaned.to_csv("sign_language_data.csv", index=False)
--- test3.py
+++ test3.py
@@ -4,8 +4,8 @@ import pandas as pd
 df = pd.read_csv("sign_language_data.csv")
 
 # Filter rows where label == "L" and flex0 > 950
-filtered = df[(df["label"] == "REST") & (df["flex1"] > 950)]
+filtered = df[(df["label"] == "D") & (df["flex0"] < 750)]
 
 print(filtered)
 
-#filtered.to_csv("filtered_output.csv", index=False)
+filtered.to_csv("filtered_output.csv", index=False)
